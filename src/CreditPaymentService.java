9e0c339e7eaad77cb45ac3a6c37294bdf942bbd9
Sergei US <106164110+QA-USV@users.noreply.github.com>
2022-06-23 12:37:35 +0300

    Formatting

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,8 +1,8 @@
 public class CreditPaymentService {
     public double calculate(int creditSum, int creditPeriod, double creditRate) {
         double ratePerMonth = creditRate / 12 / 100;
-        double x = Math.pow((1 + ratePerMonth), (creditPeriod * 12));
-        double annIndex = (ratePerMonth * x / (x - 1));
+        double countRoot = Math.pow((1 + ratePerMonth), (creditPeriod * 12));
+        double annIndex = (ratePerMonth * countRoot / (countRoot - 1));
         return annIndex * creditSum;
     }
 }
