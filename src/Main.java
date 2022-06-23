810cd45f93adb214dcaa91ad6060c10ed6f3e3fb
Sergei US <106164110+QA-USV@users.noreply.github.com>
2022-06-23 12:32:11 +0300

    Formatting

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,11 @@
 public class Main {
-
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
         int creditSum = 1_000_000; //Сумма кредита в рублях;
         int creditPeriod = 3; //Срок кредита в годах;
         double creditRate = 9.99; //Годовая ставка кредита в %;
         double sumPerMonth = service.calculate(creditSum, creditPeriod, creditRate);
-        String paymentPerMonth = String.format("Ежемесячный аннуитетный платеж по Вашему кредиту на сумму " + creditSum + " руб. на " + creditPeriod + " год(а) по ставке " + creditRate + " % годовых составит: %.0f руб.", sumPerMonth);
+        String paymentPerMonth = String.format("Ежемесячный аннуитетный платеж по Вашему кредиту на сумму " + creditSum + " руб. на " + creditPeriod + " год(а) по ставке " + creditRate + " процентов годовых составит: %.0f руб.", sumPerMonth);
         System.out.println(paymentPerMonth);
     }
 }
